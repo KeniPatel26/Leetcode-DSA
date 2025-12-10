b55ca62d50fdd1446fe12500d0cd2e0e0f425bfe
Keni Patel <164118631+KeniPatel26@users.noreply.github.com>
2025-12-10 08:06:40 +0530

    Time: 1 ms (63.10%) | Memory: 47.1 MB (48.68%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -1,9 +1,12 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
-        int m = matrix.length, n = matrix[0].length;
-        boolean firstRowZero = false, firstColZero = false;
 
-        // Step 1: Check if first row has zero
+        int m = matrix.length;
+        int n = matrix[0].length;
+
+        boolean firstRowZero = false;
+        boolean firstColZero = false;
+
         for (int j = 0; j < n; j++) {
             if (matrix[0][j] == 0) {
                 firstRowZero = true;
@@ -11,7 +14,6 @@ class Solution {
             }
         }
 
-        // Step 1: Check if first column has zero
         for (int i = 0; i < m; i++) {
             if (matrix[i][0] == 0) {
                 firstColZero = true;
@@ -19,17 +21,15 @@ class Solution {
             }
         }
 
-        // Step 2: Use first row/col as markers
         for (int i = 1; i < m; i++) {
             for (int j = 1; j < n; j++) {
                 if (matrix[i][j] == 0) {
-                    matrix[i][0] = 0;
-                    matrix[0][j] = 0;
+                    matrix[i][0] = 0; 
+                    matrix[0][j] = 0; 
                 }
             }
         }
 
-        // Step 3: Set zeroes based on markers
         for (int i = 1; i < m; i++) {
             for (int j = 1; j < n; j++) {
                 if (matrix[i][0] == 0 || matrix[0][j] == 0) {
@@ -38,14 +38,13 @@ class Solution {
             }
         }
 
-        // Step 4: Handle first row
+    
         if (firstRowZero) {
             for (int j = 0; j < n; j++) {
                 matrix[0][j] = 0;
             }
         }
 
-        // Step 4: Handle first column
         if (firstColZero) {
             for (int i = 0; i < m; i++) {
                 matrix[i][0] = 0;
